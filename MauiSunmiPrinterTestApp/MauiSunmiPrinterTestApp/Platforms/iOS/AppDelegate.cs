eeebbae5b1431d5c3afa5d116ebf880ccb5a2d7f
ppp <ppp.3ppj@gmail.com>
2023-03-12 23:46:53 +0700

    delete old app

--- MauiSunmiPrinterTestApp/MauiSunmiPrinterTestApp/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace MauiSunmiPrinterTestApp;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
